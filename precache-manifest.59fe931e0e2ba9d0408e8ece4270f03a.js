6a956d9aa2fd3cb9adddf6bdd2e8ae9cdb79022e
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-05-19 19:12:19 -0700

    Updates

---
+++ precache-manifest.59fe931e0e2ba9d0408e8ece4270f03a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5ccd46ef101f07530c5773e57edbf5",
+    "url": "/scrum-hours/index.html"
+  },
+  {
+    "revision": "617f8aa0fd06ca100ed4",
+    "url": "/scrum-hours/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fec6eade5115e271850",
+    "url": "/scrum-hours/static/js/2.2b34b1f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/scrum-hours/static/js/2.2b34b1f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617f8aa0fd06ca100ed4",
+    "url": "/scrum-hours/static/js/main.024da9dc.chunk.js"
+  },
+  {
+    "revision": "d8bf389dcc80eec9da3a",
+    "url": "/scrum-hours/static/js/runtime-main.073bbae9.js"
+  }
+]);
